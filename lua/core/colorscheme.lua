bee4ce44572ff345e54c93a6f07c5559eae1e816
Manpreet Singh <msp99000@gmail.com>
2023-08-25 17:34:59 +0530

    Removed old config files

--- lua/core/colorscheme.lua
+++
@@ -1,7 +0,0 @@
--- Set colorscheme to 'nightfly' with protected call
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
-
-if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
-end
--- lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
--- Import comment plugin safely
-local setup, comment = pcall(require, "Comment")
-
-if not setup then
-  return
-end
-
--- Enable comment
-comment.setup()
