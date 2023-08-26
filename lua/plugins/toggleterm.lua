25498b42ad0d8a58e08e42458eb078d8863bad6f
Manpreet Singh <msp99000@gmail.com>
2023-08-26 12:47:31 +0530

    Added Toggleterm plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = true,
+  opts = {
+    size = 15,
+    open_mapping = [[<S-\>]],
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = "1",
+    start_in_insert = false,
+    persist_size = true,
+    direction = "horizontal",
+  },
+}
