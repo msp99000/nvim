e9638486daec8b8bdc1a21160997b05039d667dd
Manpreet Singh <msp99000@gmail.com>
2023-08-26 12:47:00 +0530

    Added LSP folder to imports

--- lua/setup.lua
+++ lua/setup.lua
@@ -14,7 +14,8 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 -- Lazy setup
 require("lazy").setup({
   spec = {
-	  { import = "plugins" } 
+	  { import = "plugins" }, 
+	  { import = "plugins.lsp" } 
   },
   defaults = {
     lazy = false,
