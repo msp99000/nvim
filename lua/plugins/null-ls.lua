9c71a6c2e963bab8a96a73c72ebe8c1e3533d2df
Manpreet Singh <msp99000@gmail.com>
2023-08-26 11:33:08 +0530

    Added python config to null-ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,5 +1,3 @@
--- local null_ls = require("null-ls.nvim")
-
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, opts)
@@ -9,7 +7,6 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
-        
         -- Python
         null_ls.builtins.formatting.black,
 
