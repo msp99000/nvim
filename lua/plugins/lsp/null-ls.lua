37d9d2012beeff0f9d557b04a30134f581911662
Manpreet Singh <msp99000@gmail.com>
2023-08-28 23:50:56 +0530

    Added javascript, typescript, markdown, json etc formatters

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -1,40 +1,53 @@
-return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "mason.nvim" },
-  opts = function(_, opts)
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup({
-      sources = {
-        -- Lua
-        null_ls.builtins.formatting.stylua,
-        -- null_ls.builtins.diagnostics.lua_ls,
+return {}
 
-        -- Javascript
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.formatting.prettier,
-
-        -- Python
-        null_ls.builtins.formatting.black,
-        -- null_ls.builtins.diagnostics.pyright,
-        null_ls.builtins.diagnostics.ruff,
-      },
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({
-            group = augroup,
-            buffer = bufnr,
-          })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
-    })
-  end,
-}
+-- return {
+--   "jose-elias-alvarez/null-ls.nvim",
+--   event = { "BufReadPre", "BufNewFile" },
+--   lazy = false,
+--   opts = function(_, opts)
+--     local null_ls = require("null-ls")
+--     local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--     null_ls.setup({
+--       sources = {
+--         -- Lua
+--         null_ls.builtins.formatting.stylua,
+--
+--         -- Javascript
+--         null_ls.builtins.formatting.prettier,
+--         -- null_ls.builtins.formatting.prettier.with({
+--         --   filetypes = {
+--         --     "javascript",
+--         --     "typescript",
+--         --     "css",
+--         --     "scss",
+--         --     "html",
+--         --     "json",
+--         --     "yaml",
+--         --     "markdown",
+--         --     "graphql",
+--         --     "md",
+--         --     "txt",
+--         --   },
+--         -- }),
+--
+--         -- Python
+--         null_ls.builtins.formatting.black,
+--       },
+--       on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--           vim.api.nvim_clear_autocmds({
+--             group = augroup,
+--             buffer = bufnr,
+--           })
+--           vim.api.nvim_create_autocmd("BufWritePre", {
+--             group = augroup,
+--             buffer = bufnr,
+--             callback = function()
+--               vim.lsp.buf.format({ bufnr = bufnr })
+--             end,
+--           })
+--         end
+--       end,
+--     })
+--   end,
+-- }
