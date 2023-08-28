53c6094c3d7e0d6f46e009664c520b8b8fe17aa6
Manpreet Singh <msp99000@gmail.com>
2023-08-28 23:49:47 +0530

    feat: Integrated diagnostics intto buffer tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -16,26 +16,35 @@ return {
       close_icon = "",
       left_trunc_marker = "",
       right_trunc_marker = "",
+
       close_command = function(n)
         require("mini.bufremove").delete(n, false)
       end,
       right_mouse_command = function(n)
         require("mini.bufremove").delete(n, false)
       end,
-      diagnostics = "nvim_lsp",
+
+      diagnostics = "coc",
       always_show_bufferline = false,
+
       diagnostics_indicator = function(_, _, diag)
-        local icons = require("lazyvim.config").icons.diagnostics
+        local icons = {
+          Error = " ",
+          Warn = " ",
+          Hint = " ",
+          Info = " ",
+        }
         local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warn .. diag.warning or "")
+        -- .. (diag.warning and icons.Warn .. diag.warning or "")
         return vim.trim(ret)
       end,
+
       offsets = {
         {
           filetype = "neo-tree",
           text = "File Explorer",
           text_align = "center",
-          separator = true,
+          separator = false,
         },
       },
       separator_style = "slant",
