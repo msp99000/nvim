d293aa49b09407a5a9a4fd29a67d3c982837c4dd
Manpreet Singh <msp99000@gmail.com>
2023-08-28 23:50:18 +0530

    Added tsserver, emmet_ls LSP

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,27 +1,50 @@
-return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "jay-babu/mason-null-ls.nvim"
-  },
-  -- cmd = "Mason",
-  -- build = ":MasonUpdate",
-  opts = {
-    ensure_installed = {
-      "rust-analyzer",
-      -- "tsserver",
-      -- "html",
-      -- "cssls",
-      -- "tailwindcss",
-      -- "svelte",
-      -- "graphql",
-      -- "emmet_ls",
-      -- "prismals"
-      "eslint_d",
-      "prettier",
-      "black",
-      "mypy",
-      "ruff",
-    },
-  },
-}
+-- return {
+--   "williamboman/mason.nvim",
+--   dependencies = {
+--     "williamboman/mason-lspconfig.nvim",
+--     "jay-babu/mason-null-ls.nvim",
+--   },
+--   config = function()
+--     -- enable mason
+--     require("mason").setup()
+--
+--     -- import mason plugin safely
+--     local mason = require("mason")
+--
+--     -- import mason-lspconfig plugin safely
+--     local mason_lspconfig = require("mason-lspconfig")
+--
+--     -- import mason-null-ls plugin safely
+--     local mason_null_ls = require("mason-null-ls")
+--
+--
+--     mason_lspconfig.setup({
+--       -- list of servers for mason to install
+--       ensure_installed = {
+--         "tsserver",
+--         "html",
+--         "cssls",
+--         "tailwindcss",
+--         "svelte",
+--         "lua_ls",
+--         "graphql",
+--         "emmet_ls",
+--         "prismals",
+--       },
+--       -- auto-install configured servers (with lspconfig)
+--       automatic_installation = true, -- not the same as ensure_installed
+--     })
+--
+--     mason_null_ls.setup({
+--       -- list of formatters & linters for mason to install
+--       ensure_installed = {
+--         "prettier", -- ts/js formatter
+--         "stylua", -- lua formatter
+--         "eslint_d", -- ts/js linter
+--         "black", -- python formatter
+--       },
+--       -- auto-install configured servers (with lspconfig)
+--       automatic_installation = true,
+--     })
+--   end,
+-- }
