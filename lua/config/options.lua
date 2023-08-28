c8d291848bdc0b38eeea7fa288881e1ee23f7c91
Manpreet Singh <msp99000@gmail.com>
2023-08-28 23:48:04 +0530

    Added option to increase null-ls formatter time

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,12 +2,12 @@ local opt = vim.opt -- For conciseness
 
 -- Line numbers
 opt.relativenumber = true -- Show relative line numbers
-opt.number = true -- Shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- Shows absolute line number on cursor line (when relative number is on)
 
 -- Tabs & Indentation
-opt.tabstop = 2 -- Two spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- Two spaces for indent width
-opt.expandtab = true -- Expand tab to spaces
+opt.tabstop = 2       -- Two spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- Two spaces for indent width
+opt.expandtab = true  -- Expand tab to spaces
 opt.autoindent = true -- Copy indent from current line when starting new one
 
 -- Line wrapping
@@ -15,13 +15,13 @@ opt.wrap = false -- Disable line wrapping
 
 -- Search settings
 opt.ignorecase = true -- Ignore case when searching
-opt.smartcase = true -- If you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- If you include mixed case in your search, assumes you want case-sensitive
 
 -- Cursor line
 opt.cursorline = true -- Highlight the current cursor line
 
 -- Formatter timeout
-vim.lsp.buf.format({ timeout_ms = 2000 })
+vim.lsp.buf.format({ timeout_ms = 5000 })
 
 ----------------------------------------------------------
 -- Appearance
@@ -30,7 +30,7 @@ vim.lsp.buf.format({ timeout_ms = 2000 })
 -- Turn on termguicolors for tokyonight colorscheme to work
 opt.termguicolors = true
 opt.background = "dark" -- Colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- Show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- Show sign column so that text doesn't shift
 
 -- Backspace
 opt.backspace = "indent,eol,start" -- Allow backspace on indent, end of line or insert mode start position
