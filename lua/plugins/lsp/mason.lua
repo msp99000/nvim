2075bad383315fe9a6a40e5e566bcab3de7cc968
Manpreet Singh <msp99000@gmail.com>
2023-08-29 14:07:48 +0530

    build: added mason package manager along with mason-lspconfig and mason-null-ls

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,50 +1,56 @@
--- return {
---   "williamboman/mason.nvim",
---   dependencies = {
---     "williamboman/mason-lspconfig.nvim",
---     "jay-babu/mason-null-ls.nvim",
---   },
---   config = function()
---     -- enable mason
---     require("mason").setup()
---
---     -- import mason plugin safely
---     local mason = require("mason")
---
---     -- import mason-lspconfig plugin safely
---     local mason_lspconfig = require("mason-lspconfig")
---
---     -- import mason-null-ls plugin safely
---     local mason_null_ls = require("mason-null-ls")
---
---
---     mason_lspconfig.setup({
---       -- list of servers for mason to install
---       ensure_installed = {
---         "tsserver",
---         "html",
---         "cssls",
---         "tailwindcss",
---         "svelte",
---         "lua_ls",
---         "graphql",
---         "emmet_ls",
---         "prismals",
---       },
---       -- auto-install configured servers (with lspconfig)
---       automatic_installation = true, -- not the same as ensure_installed
---     })
---
---     mason_null_ls.setup({
---       -- list of formatters & linters for mason to install
---       ensure_installed = {
---         "prettier", -- ts/js formatter
---         "stylua", -- lua formatter
---         "eslint_d", -- ts/js linter
---         "black", -- python formatter
---       },
---       -- auto-install configured servers (with lspconfig)
---       automatic_installation = true,
---     })
---   end,
--- }
+return {
+  {
+  "williamboman/mason.nvim",
+  config = function()
+    -- enable mason
+    require("mason").setup()
+  end
+  },
+
+  -- Mason lspconfig
+  {
+   "williamboman/mason-lspconfig.nvim",
+
+    config = function ()
+
+      local mason_lspconfig = require("mason-lspconfig")
+
+      mason_lspconfig.setup({
+        -- list of servers for mason to install
+        ensure_installed = {
+          "tsserver",
+          "html",
+          "cssls",
+          "tailwindcss",
+          "svelte",
+          "lua_ls",
+          "graphql",
+          "emmet_ls",
+          "prismals",
+        }
+      })
+    end
+  },
+
+  -- Mason null-ls
+  {
+    "jay-babu/mason-null-ls.nvim",
+
+    config = function ()
+
+      local mason_null_ls = require("mason-null-ls")
+
+      mason_null_ls.setup({
+        -- list of formatters & linters for mason to install
+        ensure_installed = {
+          "prettier", -- ts/js formatter
+          "stylua", -- lua formatter
+          "eslint_d", -- ts/js linter
+          "black", -- python formatter
+        },
+        -- auto-install configured servers (with lspconfig)
+        automatic_installation = true,
+      })
+    end
+  }
+}
