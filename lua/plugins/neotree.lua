0abdf43f9eb0698f4eab922fad580bb327182171
Manpreet Singh <msp99000@gmail.com>
2023-08-29 14:14:27 +0530

    refactor: neotree sidebar width decreased to 30

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,40 +1,45 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    window = {
+      width = 30,
     },
-    opts = {
-      window = {
-        width = 30
+    default_component_configs = {
+      indent = {
+        with_expanders = true,
+        expander_collapsed = "",
+        expander_expanded = "",
       },
-      default_component_configs = {
-        indent = {
-          with_expanders = true,
-          expander_collapsed = "",
-          expander_expanded = ""
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted = "✖", -- this can only be used in the git_status source
+          renamed = "󰁕", -- this can only be used in the git_status source
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
         },
-        git_status = {
-            symbols = {
-              -- Change type
-              added     = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
-              modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted   = "✖",-- this can only be used in the git_status source
-              renamed   = "󰁕",-- this can only be used in the git_status source
-              -- Status type
-              untracked = "",
-              ignored   = "",
-              unstaged  = "󰄱",
-              staged    = "",
-              conflict  = "",
-            }
-          },
-        modified = {
-            symbol = "",
-            highlight = "NeoTreeModified",
-          },
-      }
-  }
+      },
+      modified = {
+        symbol = "",
+        highlight = "NeoTreeModified",
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        hide_gitignored = false,
+      },
+    },
+  },
 }
