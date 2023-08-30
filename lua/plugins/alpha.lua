fd09ce789da4739e9ad9c3f08bce1cccf3544ba5
Manpreet Singh <msp99000@gmail.com>
2023-08-30 23:25:51 +0530

    feat: add aplha welcome screen/dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,4 +1,84 @@
-return {}
+return {
+  "goolord/alpha-nvim",
+  event = "VimEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
+
+    -- Set header
+    dashboard.section.header.val = {
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "  ███╗   ██╗  ███████╗   ██████╗   ██╗   ██╗  ██╗  ███╗   ███╗ ",
+      "  ████╗  ██║  ██╔════╝  ██╔═══██╗  ██║   ██║  ██║  ████╗ ████║ ",
+      "  ██╔██╗ ██║  █████╗    ██║   ██║  ██║   ██║  ██║  ██╔████╔██║ ",
+      "  ██║╚██╗██║  ██╔══╝    ██║   ██║  ╚██╗ ██╔╝  ██║  ██║╚██╔╝██║ ",
+      "  ██║ ╚████║  ███████╗  ╚██████╔╝   ╚████╔╝   ██║  ██║ ╚═╝ ██║ ",
+      "  ╚═╝  ╚═══╝  ╚══════╝   ╚═════╝     ╚═══╝    ╚═╝  ╚═╝     ╚═╝ ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+      "                                                               ",
+    }
+
+    -- Set menu
+    dashboard.section.buttons.val = {
+      dashboard.button("n", "   New File", "<cmd>ene<CR>"),
+      dashboard.button("e", "   File Explorer", "<cmd>Neotree toggle<CR>"),
+      dashboard.button("s", "󰱼   Find File", "<cmd>Telescope find_files<CR>"),
+      dashboard.button("w", "   Find Word", "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("r", "󰁯   Restore Session", "<cmd>SessionRestore<CR>"),
+      dashboard.button("q", "   Quit NVIM", "<cmd>qa<CR>"),
+    }
+
+    -- Send config to alpha
+    alpha.setup(dashboard.opts)
+
+    -- Disable folding on alpha buffer
+    vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+  end,
+}
+
+
+
+
+-- local dashboard = {
+--
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "  ███╗   ██╗  ███████╗   ██████╗   ██╗   ██╗  ██╗  ███╗   ███╗ ",
+--   "  ████╗  ██║  ██╔════╝  ██╔═══██╗  ██║   ██║  ██║  ████╗ ████║ ",
+--   "  ██╔██╗ ██║  █████╗    ██║   ██║  ██║   ██║  ██║  ██╔████╔██║ ",
+--   "  ██║╚██╗██║  ██╔══╝    ██║   ██║  ╚██╗ ██╔╝  ██║  ██║╚██╔╝██║ ",
+--   "  ██║ ╚████║  ███████╗  ╚██████╔╝   ╚████╔╝   ██║  ██║ ╚═╝ ██║ ",
+--   "  ╚═╝  ╚═══╝  ╚══════╝   ╚═════╝     ╚═══╝    ╚═╝  ╚═╝     ╚═╝ ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--   "                                                               ",
+--
+-- }
+
+
+
+
+
+
+
+
 
 -- return {
 --   "goolord/alpha-nvim",
@@ -21,10 +101,3 @@ return {}
 --     table.insert(dashboard.section.buttons.val, 4, button)
 --   end,
 -- }
-
---      dashboard.button("n", "   New File", "<cmd>ene<CR>"),
---      dashboard.button("e", "   File Explorer", "<cmd>Neotree toggle<CR>"),
---      dashboard.button("s", "󰱼   Find File", "<cmd>Telescope find_files<CR>"),
---      dashboard.button("w", "   Find Word", "<cmd>Telescope live_grep<CR>"),
---      dashboard.button("r", "󰁯   Restore Session", "<cmd>SessionRestore<CR>"),
---      dashboard.button("q", "   Quit NVIM", "<cmd>qa<CR>"),
