abad9ef1fb90459dedd71bd96cd31c1f6bee5418
Manpreet Singh <msp99000@gmail.com>
2023-08-30 23:42:49 +0530

    style: change navigator icon to block arrow

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,12 +22,13 @@ return {
     telescope.setup({
       -- configure custom mappings
       defaults = {
-        prompt_prefix = "➤",
+        prompt_prefix = "▶ ",
+        selection_caret = "▶ ",
         path_display = { "truncate" },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
+            ["<C-j>"] = actions.move_selection_next,                           -- move to next result
             ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
           },
         },
