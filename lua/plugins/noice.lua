0d94576ed53c41edff2d1c0a04ef2da57d0e47be
Manpreet Singh <msp99000@gmail.com>
2023-08-30 23:42:09 +0530

    chore: reduced notification timeout to 1000ms

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,11 +5,12 @@ return {
     -- add any options here
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
+    {
+      "rcarriga/nvim-notify",
+      opts = {
+        timeout = 1000
+      }
     }
+  }
 }
