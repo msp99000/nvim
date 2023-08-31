86eb8cfbc83d11385eca7c3b6c2aba40a0afb51d
Manpreet Singh <msp99000@gmail.com>
2023-08-31 13:36:24 +0530

    style: change theme to nightfly

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,13 @@
 return {
-  "folke/tokyonight.nvim",
+  -- "folke/tokyonight.nvim",
+  "bluz71/vim-nightfly-colors",
   priority = 1000,
   config = function()
     -- Load colorscheme
-    vim.cmd("colorscheme tokyonight")
-    require("tokyonight").setup {
-      style = "night",
-      transparent = false,
-    }
+    vim.cmd("colorscheme nightfly")
+    -- require("tokyonight").setup {
+    --   style = "night",
+    --   transparent = false,
+    -- }
   end
 }
