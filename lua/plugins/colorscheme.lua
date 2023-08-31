b1d72ef31d00d539b0c862235e680f5746516154
Manpreet Singh <msp99000@gmail.com>
2023-08-31 13:38:22 +0530

    style: change theme back to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,12 @@
 return {
-  -- "folke/tokyonight.nvim",
-  "bluz71/vim-nightfly-colors",
+  "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
     -- Load colorscheme
-    vim.cmd("colorscheme nightfly")
-    -- require("tokyonight").setup {
-    --   style = "night",
-    --   transparent = false,
-    -- }
+    vim.cmd("colorscheme tokyonight")
+    require("tokyonight").setup {
+      style = "night",
+      transparent = false,
+    }
   end
 }
