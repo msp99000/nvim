a5260e806847f56426649c5590d894cba004ce24
Manpreet Singh <msp99000@gmail.com>
2023-08-31 22:00:41 +0530

    style: change window bg and sidebar bg to more darker colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,6 +7,16 @@ return {
     require("tokyonight").setup {
       style = "night",
       transparent = false,
+      -- styles = {
+      --   functions = {
+      --     italics = true
+      --   }
+      -- },
+      ---@param colors ColorScheme
+      on_colors = function(colors)
+        colors.bg = "#0d0e14"
+        colors.bg_sidebar = "#151724"
+      end
     }
   end
 }
