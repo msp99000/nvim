b3b4a3977a50946224c299135c36b2c464281c91
Manpreet Singh <msp99000@gmail.com>
2023-08-31 22:00:00 +0530

    chore: remove barbecue for qf plugin

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -1,88 +1,138 @@
-local icons = {
-  File = "󰈙 ",
-  Module = " ",
-  Namespace = "󰌗 ",
-  Package = " ",
-  Class = "󰌗 ",
-  Method = "󰆧 ",
-  Property = " ",
-  Field = " ",
-  Constructor = " ",
-  Enum = "󰕘",
-  Interface = "󰕘",
-  Function = "󰊕 ",
-  Variable = "󰆧 ",
-  Constant = "󰏿 ",
-  String = "󰀬  or ",
-  Number = "󰎠 ",
-  Boolean = "◩ ",
-  Array = "󰅪 ",
-  Object = "󰅩 ",
-  Key = "󰌋 ",
-  Null = "󰟢 ",
-  EnumMember = "  or ",
-  Struct = "󰌗 ",
-  Event = "  or ",
-  Operator = "󰆕 ",
-  TypeParameter = "󰊄 ",
-}
+return {}
 
-return {
-  "utilyre/barbecue.nvim",
-  name = "barbecue",
-  version = "*",
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-  opts = {
-    -- configurations go here
-    symbols = {
-      ---Modification indicator.
-      ---
-      ---@type string
-      modified = "●",
-
-      ---Truncation indicator.
-      ---
-      ---@type string
-      ellipsis = "…",
-
-      ---Entry separator.
-      ---
-      ---@type string
-      separator = "➤",
-    },
-    themes = {
-      separator = { fg = "#EC4623" },
-    },
-    kinds = {
-      File = " ",
-      Module = " ",
-      Namespace = "󰌗 ",
-      Package = " ",
-      Class = " ",
-      Method = " ",
-      Property = " ",
-      Field = " ",
-      Constructor = " ",
-      Enum = " ",
-      Interface = " ",
-      Function = "󰊕 ",
-      Variable = " ",
-      Constant = "󰏿 ",
-      String = "󰀬 ",
-      Number = " ",
-      Boolean = " ",
-      Array = " ",
-      Object = " ",
-      Key = "󰌋 ",
-      Null = "󰟢 ",
-      EnumMember = " ",
-      Struct = " ",
-      Event = " ",
-      Operator = " ",
-      TypeParameter = "󰊄 ",
-    },
-  },
-}
+-- local extra_icons = {
+--   File = "󰈙 ",
+--   Module = " ",
+--   Namespace = "󰌗 ",
+--   Package = " ",
+--   Class = "󰌗 ",
+--   Method = "󰆧 ",
+--   Property = " ",
+--   Field = " ",
+--   Constructor = " ",
+--   Enum = "󰕘",
+--   Interface = "󰕘",
+--   Function = "󰊕 ",
+--   Variable = "󰆧 ",
+--   Constant = "󰏿 ",
+--   String = "󰀬  or ",
+--   Number = "󰎠 ",
+--   Boolean = "◩ ",
+--   Array = "󰅪 ",
+--   Object = "󰅩 ",
+--   Key = "󰌋 ",
+--   Null = "󰟢 ",
+--   EnumMember = "  or ",
+--   Struct = "󰌗 ",
+--   Event = "  or ",
+--   Operator = "󰆕 ",
+--   TypeParameter = "󰊄 ",
+-- }
+--
+-- -- if 1 == 2 then
+-- --   return (extra_icons)
+-- -- end
+--
+-- return {
+--   "utilyre/barbecue.nvim",
+--   name = "barbecue",
+--   version = "*",
+--   dependencies = {
+--     "SmiteshP/nvim-navic",
+--     "nvim-tree/nvim-web-devicons", -- optional dependency
+--   },
+--
+--   opts = {
+--     -- configurations go here
+--     symbols = {
+--       ---Modification indicator.
+--       ---
+--       ---@type string
+--       modified = "●",
+--
+--       ---Truncation indicator.
+--       ---
+--       ---@type string
+--       ellipsis = "…",
+--
+--       ---Entry separator.
+--       ---
+--       ---@type string
+--       separator = "➤",
+--     },
+--
+--     config = function()
+--       local c = require("tokyonight.colors").setup({ transform = true })
+--       require("barbecue").setup({
+--         theme = {
+--           -- separator = { fg = "#EC4623" },
+--           normal = { bg = c.none, fg = c.fg_dark },
+--
+--           ellipsis = { fg = c.dark5 },
+--           separator = { fg = c.dark5 },
+--           modified = { fg = c.warning },
+--
+--           dirname = { fg = c.dark5 },
+--           basename = { fg = c.fg_dark, bold = true },
+--           context = { fg = c.fg_dark },
+--
+--           context_file = { fg = c.fg_dark },
+--           context_module = { fg = c.yellow },
+--           context_namespace = { fg = c.yellow },
+--           context_package = { fg = c.blue },
+--           context_class = { fg = c.orange },
+--           context_method = { fg = c.blue },
+--           context_property = { fg = c.green1 },
+--           context_field = { fg = c.green1 },
+--           context_constructor = { fg = c.blue },
+--           context_enum = { fg = c.orange },
+--           context_interface = { fg = c.orange },
+--           context_function = { fg = c.blue },
+--           context_variable = { fg = c.magenta },
+--           context_constant = { fg = c.magenta },
+--           context_string = { fg = c.green },
+--           context_number = { fg = c.orange },
+--           context_boolean = { fg = c.orange },
+--           context_array = { fg = c.orange },
+--           context_object = { fg = c.orange },
+--           context_key = { fg = c.purple },
+--           context_null = { fg = c.blue1 },
+--           context_enum_member = { fg = c.green1 },
+--           context_struct = { fg = c.orange },
+--           context_event = { fg = c.orange },
+--           context_operator = { fg = c.green1 },
+--           context_type_parameter = { fg = c.green1 },
+--         }
+--       })
+--     end,
+--
+--     kinds = {
+--       File = " ",
+--       Module = " ",
+--       Namespace = "󰌗 ",
+--       Package = " ",
+--       Class = " ",
+--       Method = " ",
+--       Property = " ",
+--       Field = " ",
+--       Constructor = " ",
+--       Enum = " ",
+--       Interface = " ",
+--       Function = "󰊕 ",
+--       Variable = " ",
+--       Constant = "󰏿 ",
+--       String = "󰀬 ",
+--       Number = " ",
+--       Boolean = " ",
+--       Array = " ",
+--       Object = " ",
+--       Key = "󰌋 ",
+--       Null = "󰟢 ",
+--       EnumMember = " ",
+--       Struct = " ",
+--       Event = " ",
+--       Operator = " ",
+--       TypeParameter = "󰊄 ",
+--     },
+--   },
+-- }
