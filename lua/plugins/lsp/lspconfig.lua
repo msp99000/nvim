2b1a878a82ecf482781a3ed384daad385130d714
Manpreet Singh <msp99000@gmail.com>
2023-08-31 22:01:08 +0530

    feat: added lspsaga plugin

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,76 +1,90 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-    {
-      "folke/neodev.nvim",
-      config = function()
-        require("neodev").setup()
-      end
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "VeryLazy",
+    opts = {
+      symbol_in_winbar = {
+        enable = true,
+      },
+      lightbulb = {
+        enable = true,
+      },
     },
   },
-  opts = {
-    autoformat = false,
-  },
-  config = function()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-    local lspconfig = require("lspconfig")
-    -- Lua config
-    lspconfig.lua_ls.setup({
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" }, -- Make language server recognize "vim" global
-          },
-          telemetry = { enable = false },
-          workspace = {
-            { checkThirdParty = false },
-            -- make language server aware of runtime files
-            library = {
-              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-              [vim.fn.stdpath("config") .. "/lua"] = true,
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+      {
+        "folke/neodev.nvim",
+        config = function()
+          require("neodev").setup()
+        end
+      },
+    },
+    opts = {
+      autoformat = false,
+    },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      -- Lua config
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" }, -- Make language server recognize "vim" global
+            },
+            telemetry = { enable = false },
+            workspace = {
+              { checkThirdParty = false },
+              -- make language server aware of runtime files
+              library = {
+                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                [vim.fn.stdpath("config") .. "/lua"] = true,
+              },
             },
           },
         },
-      },
-    })
+      })
 
-    -- Python config
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-      filetypes = { "python" },
-    })
+      -- Python config
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
+        filetypes = { "python" },
+      })
 
-    -- CSS config
-    require("lspconfig").cssls.setup({
-      capabilities = capabilities,
-    })
+      -- CSS config
+      require("lspconfig").cssls.setup({
+        capabilities = capabilities,
+      })
 
-    -- emmet_ls language server
-    lspconfig.emmet_ls.setup({
-      capabilities = capabilities,
-      -- on_attach = on_attach,
-      filetypes = {
-        "astro",
-        "css",
-        "eruby",
-        "html",
-        "htmldjango",
-        "javascriptreact",
-        "less",
-        "pug",
-        "sass",
-        "scss",
-        "svelte",
-        "typescriptreact",
-        "vue",
-      },
-    })
+      -- emmet_ls language server
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        -- on_attach = on_attach,
+        filetypes = {
+          "astro",
+          "css",
+          "eruby",
+          "html",
+          "htmldjango",
+          "javascriptreact",
+          "less",
+          "pug",
+          "sass",
+          "scss",
+          "svelte",
+          "typescriptreact",
+          "vue",
+        },
+      })
 
-    -- Typescript config
-    require("lspconfig").tsserver.setup({
-      capabilities = capabilities,
-    })
-  end,
+      -- Typescript config
+      require("lspconfig").tsserver.setup({
+        capabilities = capabilities,
+      })
+    end,
+  }
 }
