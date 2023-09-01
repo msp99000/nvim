5de5174832834216342e0b144e2f97ae2a374707
Manpreet Singh <msp99000@gmail.com>
2023-09-02 00:27:43 +0530

    chore: add extra icons for future use

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,3 +1,54 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "NvChad/nvim-colorizer.lua",
+
+  config = function(_, opts)
+    require("colorizer").setup(opts)
+
+    -- execute colorizer as soon as possible
+    vim.defer_fn(function()
+      require("colorizer").attach_to_buffer(0)
+    end, 0)
+  end,
 }
+
+-- Namespace = "󰌗",
+-- Text = "󰉿",
+-- Method = "󰆧",
+-- Function = "󰆧",
+-- Constructor = "",
+-- Field = "󰜢",
+-- Variable = "󰀫",
+-- Class = "󰠱",
+-- Interface = "",
+-- Module = "",
+-- Property = "󰜢",
+-- Unit = "󰑭",
+-- Value = "󰎠",
+-- Enum = "",
+-- Keyword = "󰌋",
+-- Snippet = "",
+-- Color = "󰏘",
+-- File = "󰈚",
+-- Reference = "󰈇",
+-- Folder = "󰉋",
+-- EnumMember = "",
+-- Constant = "󰏿",
+-- Struct = "󰙅",
+-- Event = "",
+-- Operator = "󰆕",
+-- TypeParameter = "󰊄",
+-- Table = "",
+-- Object = "󰅩",
+-- Tag = "",
+-- Array = "[]",
+-- Boolean = "",
+-- Number = "",
+-- Null = "󰟢",
+-- String = "󰉿",
+-- Calendar = "",
+-- Watch = "󰥔",
+-- Package = "",
+-- Copilot = "",
+-- Codeium = "",
+-- TabNine = "",
+
