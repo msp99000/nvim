da3278093d6cfa76eb425e4d393cab44e3b77d26
Manpreet Singh <msp99000@gmail.com>
2023-09-02 23:32:14 +0530

    feat: add diagnostics symbols in neotree

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -27,8 +27,15 @@ return {
       autoformat = false,
     },
     config = function()
+      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
+
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
+
       -- Lua config
       lspconfig.lua_ls.setup({
         settings = {
