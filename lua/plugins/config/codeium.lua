82a146be66dde77363e2c0330eed5825c37dab3a
Manpreet Singh <msp99000@gmail.com>
2023-09-08 19:16:38 +0530

    feat(codeium): adds keymaps for code suggestion traversal in insert mode

---
+++ lua/plugins/config/codeium.lua
@@ -0,0 +1,20 @@
+local codeium = require("codeium.vim")
+
+codeium.setup({
+	-- Change '<C-g>' here to any keycode you like.
+	vim.keymap.set("i", "<C-g>", function()
+		return vim.fn["codeium#Accept"]()
+	end, { expr = true }),
+
+	vim.keymap.set("i", "<c-]>", function()
+		return vim.fn["codeium#CycleCompletions"](1)
+	end, { expr = true }),
+
+	vim.keymap.set("i", "<c-[>", function()
+		return vim.fn["codeium#CycleCompletions"](-1)
+	end, { expr = true }),
+
+	vim.keymap.set("i", "<c-x>", function()
+		return vim.fn["codeium#Clear"]()
+	end, { expr = true }),
+})
