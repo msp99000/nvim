70e2f66558a2cb7833d925addbe5753e5b93ab60
Manpreet Singh <msp99000@gmail.com>
2023-09-12 19:04:51 +0530

    build: automate preconfigured neovim installation

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 # MacOS installation
 brew uninstall neovim && brew cleanup
 
+# Remove the Neovim files, swap files and cache
 echo("Removing the Neovim files, swap files and cache")
-
 rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim
 
+# Install Neovim
 echo("Reinstalling Neovim")
-
 brew install neovim
 
+# Clone the repo
 echo("Cloning the repo")
-
 git clone https://github.com/meuralnetwork/nvim.git ~/.config/nvim
