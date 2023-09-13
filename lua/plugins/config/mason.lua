39b162e9a3443991695d82fb9b5c9bc51bd2d566
Manpreet Singh <msp99000@gmail.com>
2023-09-13 23:46:57 +0530

    build(mason): adds mason-null-ls auto formatting to html-lsp-server

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -8,42 +8,42 @@ local mason_null_ls = require("mason-null-ls")
 
 -- enable mason and configure icons
 mason.setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗"
-    }
-  }
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
 mason_lspconfig.setup({
-  -- list of servers for mason to install
-  ensure_installed = {
-    "tsserver",
-    "html",
-    "cssls",
-    "tailwindcss",
-    "svelte",
-    "lua_ls",
-    "graphql",
-    "emmet_ls",
-    "prismals",
-    "pyright",
-    "rust_analyzer"
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+	-- list of servers for mason to install
+	ensure_installed = {
+		"tsserver",
+		"html",
+		"cssls",
+		"tailwindcss",
+		"svelte",
+		"lua_ls",
+		"graphql",
+		"emmet_ls",
+		"prismals",
+		"pyright",
+		"rust_analyzer",
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true, -- not the same as ensure_installed
 })
 
 mason_null_ls.setup({
-  -- list of formatters & linters for mason to install
-  ensure_installed = {
-    "prettier", -- ts/js formatter
-    "stylua",   -- lua formatter
-    "eslint_d", -- ts/js linter
-    "black",    -- python formatter
-  },
-  -- auto-install configured servers (with lspconfig)
-  automatic_installation = true,
+	-- list of formatters & linters for mason to install
+	ensure_installed = {
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"eslint_d", -- ts/js linter
+		"black", -- python formatter
+	},
+	-- auto-install configured servers (with lspconfig)
+	automatic_installation = true,
 })
