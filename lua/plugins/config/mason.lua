6674d0c1eb0c746da96db302945de1c3a079f157
Manpreet Singh <msp99000@gmail.com>
2024-04-19 13:17:00 +0530

    feat(mason tool installer): adds mason tool installer to mason set and updates the ensure installed list

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -32,7 +32,7 @@ mason_lspconfig.setup({
 		"emmet_ls",
 		"prismals",
 		"pyright",
-		"rust-analyzer",
+		"rust_analyzer",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
@@ -57,5 +57,7 @@ mason_tool_installer.setup({
 		"stylua",
 		"isort",
 		"black",
+		"pylint",
+		"eslint_d",
 	},
 })
