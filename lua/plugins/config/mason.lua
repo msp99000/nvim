d766725a4685ef53ddbc14bd0fbbca6ffb92363e
Manpreet <er.gauravpassi@gmail.com>
2024-09-12 00:16:17 +0530

    chore: updates Mason dependencies

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -22,17 +22,17 @@ mason.setup({
 mason_lspconfig.setup({
 	-- list of servers for mason to install
 	ensure_installed = {
-		"tsserver",
-		"html",
-		"cssls",
-		"tailwindcss",
-		"svelte",
-		"lua_ls",
-		"graphql",
-		"emmet_ls",
-		"prismals",
-		"pyright",
-		"rust_analyzer",
+		-- "tsserver",
+		-- "html",
+		-- "cssls",
+		-- "tailwindcss",
+		-- "svelte",
+		-- "lua_ls",
+		-- "graphql",
+		-- "emmet_ls",
+		-- "prismals",
+		-- "pyright",
+		-- "rust_analyzer",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
@@ -41,11 +41,11 @@ mason_lspconfig.setup({
 mason_null_ls.setup({
 	-- list of formatters & linters for mason to install
 	ensure_installed = {
-		"prettier", -- ts/js formatter
-		"prettierd",
-		"stylua", -- lua formatter
-		"eslint_d", -- ts/js linter
-		"black", -- python formatter
+		-- "prettier", -- ts/js formatter
+		-- "prettierd",
+		-- "stylua", -- lua formatter
+		-- "eslint_d", -- ts/js linter
+		-- "black", -- python formatter
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true,
@@ -53,11 +53,11 @@ mason_null_ls.setup({
 
 mason_tool_installer.setup({
 	ensure_installed = {
-		"prettier",
-		"stylua",
-		"isort",
-		"black",
-		"pylint",
-		"eslint_d",
+		-- "prettier",
+		-- "stylua",
+		-- "isort",
+		-- "black",
+		-- "pylint",
+		-- "eslint_d",
 	},
 })
