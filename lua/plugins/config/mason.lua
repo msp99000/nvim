37e5f85c650af8f9115dd66b9713756c5be6f689
Manpreet Singh <msp99000@gmail.com>
2025-05-21 11:50:27 +0530

    build: updates mason plugin

--- lua/plugins/config/mason.lua
+++ lua/plugins/config/mason.lua
@@ -3,9 +3,6 @@ local mason = require("mason")
 -- import mason-lspconfig
 local mason_lspconfig = require("mason-lspconfig")
 
--- import mason-null-ls
-local mason_null_ls = require("mason-null-ls")
-
 local mason_tool_installer = require("mason-tool-installer")
 
 -- enable mason and configure icons
@@ -38,26 +35,15 @@ mason_lspconfig.setup({
 	automatic_installation = true, -- not the same as ensure_installed
 })
 
-mason_null_ls.setup({
-	-- list of formatters & linters for mason to install
-	ensure_installed = {
-		-- "prettier", -- ts/js formatter
-		-- "prettierd",
-		-- "stylua", -- lua formatter
-		-- "eslint_d", -- ts/js linter
-		-- "black", -- python formatter
-	},
-	-- auto-install configured servers (with lspconfig)
-	automatic_installation = true,
-})
-
 mason_tool_installer.setup({
 	ensure_installed = {
-		-- "prettier",
-		-- "stylua",
-		-- "isort",
-		-- "black",
-		-- "pylint",
-		-- "eslint_d",
+		"prettier", -- ts/js formatter
+		"stylua", -- lua formatter
+		"black", -- python formatter
+		"isort", -- python import sorter
+		"pylint", -- python linter
+		"eslint_d", -- ts/js linter
 	},
+	auto_update = true,
+	run_on_start = true,
 })
