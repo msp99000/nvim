a39408b20e42302155771ecf261d4608a5ed9e49
Manpreet Singh <msp99000@gmail.com>
2025-05-25 14:18:40 +0530

    feat: Multi OS Support (Mac / Ubuntu)

--- install.sh
+++ install.sh
@@ -1,16 +1,26 @@
-!# bin/bash
+#!/usr/bin/env bash
 
-# Installation for MacOS
-brew uninstall neovim && brew cleanup
+OS=$(uname)
 
-# Remove Neovim files, swap files and cache
-echo("Removing all Neovim files, swap files and cache")
-rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim
+if [ "$OS" == "Darwin" ]; then
+    echo "Detected macOS. Uninstalling Neovim via brew..."
+    brew uninstall neovim && brew cleanup
+    echo "Removing all Neovim files, swap files and cache..."
+    rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim
+    echo "Reinstalling Neovim via brew..."
+    brew install neovim
+elif [ "$OS" == "Linux" ]; then
+    echo "Detected Linux. Uninstalling Neovim via apt-get..."
+    sudo apt-get remove neovim -y
+    sudo apt-get autoremove -y
+    echo "Removing all Neovim files, swap files and cache..."
+    rm -rf ~/.config/nvim ~/.local/share/nvim ~/.local/state/nvim
+    echo "Reinstalling Neovim via apt-get..."
+    sudo apt-get update && sudo apt-get install neovim -y
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
 
-# Reinstall Neovim
-echo("Reinstalling Neovim")
-brew install neovim
-
-# Clone the repo
-echo("Cloning the repo")
+echo "Cloning the repo..."
 git clone https://github.com/msp99000/nvim.git ~/.config/nvim
